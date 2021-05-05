bd8b133a46ce0806308499a48b62143e06df4323
Echonox <angelkomyou@gmail.com>
2021-05-05 12:13:28 +0200

    Return unknown in case regionCode is not available

--- src/ios/CDVLocale.swift
+++ src/ios/CDVLocale.swift
@@ -4,7 +4,7 @@ import Foundation
 public class CDVLocale : CDVPlugin {
   @objc
   func execute(_ command: CDVInvokedUrlCommand) {
-    let result: [String: Any] = ["region": NSLocale.current.regionCode!, "language": NSLocale.current.languageCode!, "preferredLanguages": NSLocale.preferredLanguages];
+    let result: [String: Any] = ["region": NSLocale.current.regionCode!, "language": NSLocale.current.regionCode ?? "unknown", "preferredLanguages": NSLocale.preferredLanguages];
     let pluginResult:CDVPluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: result)
 
     self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
